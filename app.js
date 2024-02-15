f21edff49490ff14f9851916631278dc5a6c8639
mayur11 <mayurchauhan1107@gmail.com>
2024-02-15 17:02:18 +0530

    Added all the files

--- app.js
+++ app.js
@@ -2,42 +2,37 @@ const jokeGenrateBtn = document.querySelector("#generate-btn");
 const joke = document.querySelector("#joke");
 const emoji = document.querySelector("span");
 
-const generateJoke =  async () => {
-    const res = await fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky');
-    const data = await res.json();
-    
-    // console.log(data);
-    let jokeCategory = data.category;
-    // console.log(jokeCategory);
-    if(jokeCategory == 'Programming') {
-        emoji.textContent = "🖥️";
-    } else if(jokeCategory == 'Misc') {
-        emoji.textContent = "🔀";
-    }  else if(jokeCategory == 'Dark') {
-        emoji.textContent = "💀";
-    } else if(jokeCategory == 'Pun') {
-        emoji.textContent = "🤪";
-    } else {
-        emoji.textContent = "👻";
-    } 
-    const jokeType = data.type;
-    // console.log(jokeType);
+const generateJoke = async () => {
+  const res = await fetch(
+    "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky"
+  );
+  const data = await res.json();
 
-    const jokeText = data.joke;
-    // console.log(jokeText);
+  let jokeCategory = data.category;
 
-    const jokeSetup = data.setup;
-    // console.log(jokeSetup);
+  if (jokeCategory == "Programming") {
+    emoji.textContent = "🖥️";
+  } else if (jokeCategory == "Misc") {
+    emoji.textContent = "🔀";
+  } else if (jokeCategory == "Dark") {
+    emoji.textContent = "💀";
+  } else if (jokeCategory == "Pun") {
+    emoji.textContent = "🤪";
+  } else {
+    emoji.textContent = "👻";
+  }
 
-    const jokeDelivery = data.delivery;
-    // console.log(jokeDelivery);
-    
-    if ( jokeType == 'twopart') {
-        joke.innerHTML = jokeSetup + "<br><br>" + jokeDelivery;
-    } else {
-        joke.textContent = jokeText;
-    }
-}
+  const jokeType = data.type;
+  const jokeText = data.joke;
+  const jokeSetup = data.setup;
+  const jokeDelivery = data.delivery;
 
-jokeGenrateBtn.addEventListener('click', generateJoke);
+  if (jokeType == "twopart") {
+    joke.innerHTML = jokeSetup + "<br><br>" + jokeDelivery;
+  } else {
+    joke.textContent = jokeText;
+  }
+};
+
+jokeGenrateBtn.addEventListener("click", generateJoke);
 generateJoke();
